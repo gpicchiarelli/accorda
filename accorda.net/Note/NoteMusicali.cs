65b7f05d53ce77428091309514991d0b129fd729
Giacomo Picchiarelli <gpicchiarelli@gmail.com>
2023-08-13 19:26:31 +0200

    Costanti su note musicali

---
+++ accorda.net/Note/NoteMusicali.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace accorda
+{
+    public class NoteMusicali
+    {
+        public const double Mi_Basso = 82.41;
+        public const double La = 110;
+        public const double Re = 146.83;
+        public const double Sol = 196;
+        public const double Si = 246.94;
+        public const double Mi_Alto = 329.63;
+    }
+}
