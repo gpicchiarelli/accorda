4f8d015b2d604dad5602121fb97521fb469e6b21
Giacomo Picchiarelli <gpicchiarelli@gmail.com>
2023-08-31 17:03:13 +0200

    Changes to be committed: 	new file:   accorda.net/obj/Debug/net7.0-windows/Accorda.csproj.AssemblyReference.cache 	modified:   accorda.net/obj/Debug/net7.0-windows/accorda.AssemblyInfo.cs 	modified:   accorda.net/obj/Debug/net7.0-windows/accorda.AssemblyInfoInputs.cache 	modified:   accorda.net/obj/Debug/net7.0-windows/accorda.GeneratedMSBuildEditorConfig.editorconfig 	modified:   accorda.net/obj/Debug/net7.0-windows/accorda.assets.cache 	modified:   accorda.net/obj/accorda.csproj.nuget.dgspec.json 	modified:   accorda.net/obj/accorda.csproj.nuget.g.props 	modified:   accorda.net/obj/project.assets.json 	modified:   accorda.net/obj/project.nuget.cache

--- accorda.net/obj/Debug/net7.0-windows/accorda.AssemblyInfo.cs
+++ accorda.net/obj/Debug/net7.0-windows/accorda.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("accorda")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Accorda")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("accorda")]
-[assembly: System.Reflection.AssemblyTitleAttribute("accorda")]
+[assembly: System.Reflection.AssemblyProductAttribute("Accorda")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Accorda")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
