8bc17e6188760b91881eaca5210f26d30ecc492e
Giacomo Picchiarelli <gpicchiarelli@gmail.com>
2023-09-01 11:40:35 +0200

    Allineamento a build funzionante.

---
+++ TestAccorda/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TestAccorda
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
