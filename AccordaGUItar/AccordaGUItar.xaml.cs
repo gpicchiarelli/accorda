cfe5f114b21d45a93591bae976e404f7e05c63dd
Giacomo Picchiarelli <gpicchiarelli@gmail.com>
2023-10-29 12:03:21 +0100

    Prototipo 2 GUI

--- AccordaGUItar/AccordaGUItar.xaml.cs
+++ AccordaGUItar/AccordaGUItar.xaml.cs
@@ -4,6 +4,7 @@ using NAudio.Wave;
 using System.Collections.Generic;
 using System.Windows.Controls;
 using System.Windows.Media;
+using accorda.Note;
 
 namespace Accorda
 {
@@ -58,7 +59,105 @@ namespace Accorda
             {
                 SelezionaCorda.SelectedIndex = -1; // Imposta la selezione su -1 per disabilitare la selezione della riga vuota
             }
+            else
+            {
+                // Calcola la soglia in base alla corda selezionata
+                ComboBoxItem cordaSelezionata = (ComboBoxItem)SelezionaCorda.SelectedItem;
+                string cordaInfo = cordaSelezionata.Content.ToString();
+                double soglia = CalcolaSogliaAccordatura(cordaInfo);
+
+                // Avvia l'accordatura con la nuova soglia
+                AvviaAccordatura(soglia);
+            }
         }
+
+        private double CalcolaSogliaAccordatura(string cordaInfo)
+        {
+            double soglia = 0.0;
+            if (cordaInfo.Contains("Mi (alto)"))
+            {
+                soglia = 1.0; // Imposta la soglia per la corda 1
+            }
+            else if (cordaInfo.Contains("Si"))
+            {
+                soglia = 2.0; // Imposta la soglia per la corda 2
+            }
+            else if (cordaInfo.Contains("Sol"))
+            {
+                soglia = 3.0; // Imposta la soglia per la corda 3
+            }
+            else if (cordaInfo.Contains("Re"))
+            {
+                soglia = 4.0; // Imposta la soglia per la corda 4
+            }
+            else if (cordaInfo.Contains("La"))
+            {
+                soglia = 5.0; // Imposta la soglia per la corda 5
+            }
+            else if (cordaInfo.Contains("Mi (basso)"))
+            {
+                soglia = 6.0; // Imposta la soglia per la corda 6
+            }
+            return soglia;
+        }
+        private void AvviaAccordatura(double soglia)
+        {
+            // Ottieni la frequenza corrente
+            double currentFrequency = double.Parse(FrequenzaAttuale.Text);
+
+            // Calcola la differenza tra la frequenza corrente e la frequenza target
+            double frequencyDifference = Math.Abs(currentFrequency - GetTargetFrequency());
+
+            // Verifica se la frequenza corrente è all'interno della soglia specificata
+            if (frequencyDifference <= soglia)
+            {
+                // La frequenza è nell'intervallo di accordatura
+                // Puoi eseguire le azioni desiderate, ad esempio, cambiare il colore della barra di progresso a verde
+                AccordaturaProgressBar.Value = 100; // Barra di progresso completa
+                AccordaturaProgressBar.Foreground = Brushes.Green; // Imposta il colore a verde
+            }
+            else
+            {
+                // La frequenza è al di fuori dell'intervallo di accordatura
+                // Puoi eseguire le azioni desiderate, ad esempio, cambiare il colore della barra di progresso a rosso
+                AccordaturaProgressBar.Value = 0; // Barra di progresso vuota
+                AccordaturaProgressBar.Foreground = Brushes.Red; // Imposta il colore a rosso
+            }
+        }
+
+        private double GetTargetFrequency()
+        {
+            ComboBoxItem cordaSelezionata = (ComboBoxItem)SelezionaCorda.SelectedItem;
+            string cordaInfo = cordaSelezionata.Content.ToString();
+
+            if (cordaInfo.Contains("Mi (alto)"))
+            {
+                return NoteMusicali.Mi_Alto;
+            }
+            else if (cordaInfo.Contains("Si"))
+            {
+                return NoteMusicali.Si;
+            }
+            else if (cordaInfo.Contains("Sol"))
+            {
+                return NoteMusicali.Sol;
+            }
+            else if (cordaInfo.Contains("Re"))
+            {
+                return NoteMusicali.Re;
+            }
+            else if (cordaInfo.Contains("La"))
+            {
+                return NoteMusicali.La;
+            }
+            else if (cordaInfo.Contains("Mi (basso)"))
+            {
+                return NoteMusicali.Mi_Basso;
+            }
+
+            return 0.0; // Valore predefinito se non viene riconosciuta una corda
+        }
+
 
         private void AccordaturaProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
@@ -75,6 +174,5 @@ namespace Accorda
                 AccordaturaProgressBar.Foreground = new SolidColorBrush(Colors.Red); // Colore rosso per l'accordatura errata
             }
         }
-
     }
 }
