57b1875f763da5d09459c065a199178556a86860
Giacomo Picchiarelli <gpicchiarelli@gmail.com>
2023-11-30 15:54:14 +0100

    Changes to be committed: 	modified:   AccordaGUItar/obj/Debug/net7.0-windows/AccordaGUItar.g.i.cs 	modified:   AccordaGUItar/obj/Debug/net7.0-windows/AccordaGUItar_MarkupCompile.i.cache 	modified:   AccordaGUItar/obj/Debug/net7.0-windows/App.g.i.cs 	modified:   accorda.net/.vs/Accorda/DesignTimeBuild/.dtbcache.v2 	deleted:    accorda.net/.vs/Accorda/FileContentIndex/1cb6f00d-938c-4cb4-914a-487c78386558.vsidx 	deleted:    accorda.net/.vs/Accorda/FileContentIndex/6c28fc4a-643b-4a83-8281-3fa68af1134b.vsidx 	new file:   accorda.net/.vs/Accorda/FileContentIndex/977cc5e8-412d-464b-82f9-37f7b16884f8.vsidx 	deleted:    accorda.net/.vs/Accorda/FileContentIndex/a8cb9d7a-80d5-40cf-94d0-708ddffbc155.vsidx 	deleted:    accorda.net/.vs/Accorda/FileContentIndex/c6bf62b3-6d18-4b11-9f8d-3cf64c7f6949.vsidx 	renamed:    accorda.net/.vs/Accorda/FileContentIndex/dc906572-c45c-4125-8492-9205ba43fed0.vsidx -> accorda.net/.vs/Accorda/FileContentIndex/ff7ba8b2-4908-4281-a834-53853892a3fa.vsidx 	modified:   accorda.net/.vs/Accorda/v17/.suo 	modified:   accorda.net/.vs/ProjectEvaluation/accorda.metadata.v7.bin 	modified:   accorda.net/.vs/ProjectEvaluation/accorda.projects.v7.bin

--- AccordaGUItar/obj/Debug/net7.0-windows/App.g.i.cs
+++ AccordaGUItar/obj/Debug/net7.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace AccordaGUItar {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.14.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace AccordaGUItar {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.14.0")]
         public static void Main() {
             AccordaGUItar.App app = new AccordaGUItar.App();
             app.InitializeComponent();
