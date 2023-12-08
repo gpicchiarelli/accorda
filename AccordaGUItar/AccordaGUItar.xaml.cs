0a2fb11baded68997c2b38377f8345ba15ac60c1
Giacomo Picchiarelli <gpicchiarelli@gmail.com>
2023-12-08 11:48:36 +0100

    Pulizia soluzione e ricostruzione.

--- AccordaGUItar/AccordaGUItar.xaml.cs
+++ AccordaGUItar/AccordaGUItar.xaml.cs
@@ -128,5 +128,10 @@ namespace Accorda
         {
 
         }
+
+        private void MetroProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
     }
 }
