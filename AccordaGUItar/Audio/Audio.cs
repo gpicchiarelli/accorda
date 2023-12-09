99043929bc6e86eafcb8804520f34f532dae4e15
Giacomo Picchiarelli <gpicchiarelli@gmail.com>
2023-12-09 20:05:14 +0100

    Sistemazione calcolo FFT con libreria Mathnet

--- AccordaGUItar/Audio/Audio.cs
+++ AccordaGUItar/Audio/Audio.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Windows.Forms;
-using MathNet.Numerics;
 using MathNet.Numerics.IntegralTransforms;
-using MathNet.Numerics.Statistics;
+using MathNet.Numerics;
 using NAudio.Dsp;
 using NAudio.Wave;
-
 
 namespace AccordaGUItar.Audio
 {
@@ -15,10 +12,9 @@ namespace AccordaGUItar.Audio
     {
         private readonly WaveInEvent waveIn;
         private const int sampleRate = 44100;
-        private const int bufferSize = 2048;
-        private readonly float[] buffer;
+        private const int bufferSize = 4096;
+        private readonly double[] buffer;
         private readonly Complex[] complexBuffer;
-        private readonly BiQuadFilter filter;
 
         // Evento per notificare la frequenza media calcolata
         public event EventHandler<double> SmoothedFrequencyDetected;
@@ -31,112 +27,50 @@ namespace AccordaGUItar.Audio
             waveIn = new WaveInEvent
             {
                 DeviceNumber = InputDeviceSelector,
-                BufferMilliseconds = bufferSize * 1000 * 2 / sampleRate,
+                BufferMilliseconds = bufferSize * 1000 / sampleRate,
                 WaveFormat = new WaveFormat(sampleRate, 1)
             };
 
-            filter = BiQuadFilter.LowPassFilter(44100, 20000, 1);
-
-            buffer = new float[bufferSize];
+            buffer = new double[bufferSize];
             complexBuffer = new Complex[bufferSize];
             waveIn.DataAvailable += WaveIn_DataAvailable;
             StartRecording();
         }
 
+        private double CalculateFrequencyFromFFT(double[] buffer)
+        {
+            Complex32[] fftBuffer = new Complex32[buffer.Length];
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                fftBuffer[i] = new Complex32((float)buffer[i], 0);
+            }
+
+            Fourier.Forward(fftBuffer, FourierOptions.NoScaling);
+
+            int indexOfMaxValue = fftBuffer.Select((value, index) => new { Value = value.Magnitude, Index = index })
+                                            .OrderByDescending(x => x.Value)
+                                            .First().Index;
+
+            double frequency = indexOfMaxValue * sampleRate / buffer.Length;
+            return frequency;
+        }
+
         private void WaveIn_DataAvailable(object sender, WaveInEventArgs e)
         {
-            if (e.Buffer.Length >= 2 * bufferSize)
-            {
-                double maxVolume = CalculateMaxVolume(e);
-                if (maxVolume > volumeThreshold)
-                {
-                    double[] autocorrelation = CalculateAutocorrelation(e.Buffer);
-                    double[] autocorrelationAbsolutes = new double[autocorrelation.Length];
-                    for(int u = 0; u < autocorrelation.Length; u++) {
-                        autocorrelationAbsolutes[u] = Math.Abs(autocorrelation[u]);
-                    }
-                    double averageCorrelationValue = autocorrelationAbsolutes.Average();
-                    if (averageCorrelationValue > correlationThreshold)
-                    {
-                        byte[] audioBuffer = e.Buffer; // Esempio di buffer audio
-                        Complex32[] complexBuffer = new Complex32[bufferSize];
-
-                        // Converti i dati audio in formato complesso
-                        for (int i = 0; i < bufferSize; i++)
-                        {
-                            short sample = (short)((audioBuffer[(2 * i) + 1] << 8) | audioBuffer[2 * i]);
-                            complexBuffer[i] = new Complex32(sample / (float)short.MaxValue, 0);
-                        }
-
-                        // Esegui la FFT
-                        Fourier.Forward(complexBuffer, FourierOptions.Default);
-
-                        // Calcola lo spettro delle frequenze
-                        double[] spectrum = new double[bufferSize / 2];
-                        for (int i = 0; i < bufferSize / 2; i++)
-                        {
-                            spectrum[i] = complexBuffer[i].MagnitudeSquared();
-                        }
-
-                        // Trova la frequenza dominante nello spettro
-                        int dominantIndex = spectrum.ToList().IndexOf(spectrum.Max());
-                        double dominantFrequency = (double)dominantIndex * sampleRate / bufferSize;
-
-                        // Calcola la frequenza media delle altre frequenze rispetto alla dominante
-                        double sum = 0.0;
-                        int count = 0;
-
-                        for (int i = 0; i < spectrum.Length; i++)
-                        {
-                            if (i != dominantIndex)
-                            {
-                                sum += (double)i * sampleRate / bufferSize;
-                                count++;
-                            }
-                        }
-                        double averageFrequency = sum / count;
-                        SmoothedFrequencyDetected?.Invoke(this, averageFrequency);
-                    }
-                }
-            }
-        }
-
-        private const int correlationWindowSize = bufferSize; // Dimensione della finestra per l'autocorrelazione
-        private const double correlationThreshold = 0.80; // Soglia di autocorrelazione
-
-        public double[] CalculateAutocorrelation(byte[] buffer)
-        {
-            double[] samples = new double[correlationWindowSize];
-
-            // Copia i dati audio nel buffer in un array di double normalizzato
-            for (int i = 0; i < correlationWindowSize / 2; i++)
-            {
-                short sample = (short)((buffer[(2 * i) + 1] << 8) | buffer[2 * i]);
-                samples[i] = (double)sample / short.MaxValue;
-            }
-            // Calcola la funzione di autocorrelazione usando MathNet.Numerics
-            double[] autocorrelation = Correlation.Auto(samples);
-            return autocorrelation;
-        }
-
-        private double CalculateMaxVolume(WaveInEventArgs e)
-        {
-            double maxVolume = 0.0;
-
             for (int i = 0; i < e.BytesRecorded / 2; i++)
             {
                 short sample = (short)((e.Buffer[(2 * i) + 1] << 8) | e.Buffer[2 * i]);
-                double normalizedSample = (double)sample / short.MaxValue;
-                double volume = Math.Abs(normalizedSample);
-
-                if (volume > maxVolume)
-                {
-                    maxVolume = volume;
-                }
+                buffer[i] = (double)sample / short.MaxValue;
             }
 
-            return maxVolume;
+            double maxVolume = buffer.Max(Math.Abs);
+            if (maxVolume > volumeThreshold)
+            {
+                double frequency = CalculateFrequencyFromFFT(buffer);
+                SmoothedFrequencyDetected?.Invoke(this, frequency);
+            }
         }
+
 
         public List<string> ElencaDispositiviIngresso()
         {
@@ -148,6 +82,26 @@ namespace AccordaGUItar.Audio
                 dispositivi.Add($"Dispositivo {deviceIndex + 1}: {deviceInfo.ProductName}");
             }
             return dispositivi;
+        }
+
+        private double[] CalculateAutocorrelation(double[] buffer)
+        {
+            double[] autocorrelation = new double[buffer.Length];
+            for (int lag = 0; lag < buffer.Length; lag++)
+            {
+                for (int i = 0; i < buffer.Length - lag; i++)
+                {
+                    autocorrelation[lag] += buffer[i] * buffer[i + lag];
+                }
+            }
+            return autocorrelation;
+        }
+
+        private int FindPeakFrequency(double[] autocorrelation)
+        {
+            double maxValue = autocorrelation.Max();
+            int peakIndex = Array.IndexOf(autocorrelation, maxValue);
+            return peakIndex != -1 ? peakIndex : 0;
         }
 
         private void StartRecording()
--- accorda.net/obj/Debug/net8.0-windows7.0/Accorda.AssemblyInfo.cs
+++ accorda.net/obj/Debug/net8.0-windows7.0/Accorda.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Accorda")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c0d773a250516d0eb3201d810df93da6ea8d365")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21094376eac472bf9ab987cc008e249a980e6381")]
 [assembly: System.Reflection.AssemblyProductAttribute("Accorda")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Accorda")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
