166cb59717d4d0a0d996652a4a39125d8a43cb45
Giacomo Picchiarelli <gpicchiarelli@gmail.com>
2024-01-28 18:28:05 +0100

    Update AssemblyInfo version

--- AccordaGUItar/obj/Debug/net8.0-windows7.0/AccordaGUItar.AssemblyInfo.cs
+++ AccordaGUItar/obj/Debug/net8.0-windows7.0/AccordaGUItar.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AccordaGUItar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dff57221c75a3b3e5a8773cb672f8583ebc76b5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baa8834e9689323c11d5476ffa4d456df47f666e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AccordaGUItar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AccordaGUItar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
